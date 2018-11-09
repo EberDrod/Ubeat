0a612c61e2889a9711895cd5e4acb3a5cb911a47
eberPeña <eberg.pena@gmail.com>
2018-11-09 01:37:38 -0600

    Initial Commit

---
+++ UbeatTests/UbeatTests.swift
@@ -0,0 +1,34 @@
+//
+//  UbeatTests.swift
+//  UbeatTests
+//
+//  Created by SBL on 11/9/18.
+//  Copyright © 2018 SBL. All rights reserved.
+//
+
+import XCTest
+@testable import Ubeat
+
+class UbeatTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
